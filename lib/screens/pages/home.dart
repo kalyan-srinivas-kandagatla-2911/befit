1dc8f6410699d8ea543ab4323bd36f7555cf8960
Kalyan Srinivas Kandagatla <kalyansrinivas2911@gmail.com>
2023-12-15 10:31:13 +0530

    added home page

--- lib/screens/pages/home.dart
+++ lib/screens/pages/home.dart
@@ -1,16 +1,52 @@
-
 import 'package:flutter/material.dart';
 
+class Home extends StatefulWidget {
+  @override
+  _GoalPageState createState() => _GoalPageState();
+}
 
-class Home extends StatelessWidget {
-  const Home ({Key? key}) : super(key: key);
+class _GoalPageState extends State<Home> {
+  double dailyGoal = 1000; // Set your daily goal here
+  double dailyValue = 700; // Set the user's daily value here
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Colors.green,
+        title: Text('Daily Goal Progress'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Daily Goal: $dailyGoal',
+              style: TextStyle(fontSize: 20.0),
+            ),
+              Text(
+                  'Daily Progress: $dailyValue',
+
+                  style: TextStyle(fontSize: 20.0),
+            ),
+
+            SizedBox(height: 70.0),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                CircularProgressIndicator(
+                  value: dailyValue / dailyGoal,
+                  backgroundColor: Colors.grey,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                  strokeWidth: 100.0,
+                ),
+                // Text(
+                //   '${dailyValue.toInt()} / $dailyGoal',
+                //   style: TextStyle(fontSize: 20.0),
+                // ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
